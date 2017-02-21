faf0320b1d33b34d1bd1c2889ff0b9f24ccd2bed
Chris Baronavski <cbaronavski@wamu.org>
2017-02-21 16:51:04 -0500

    update to use cdn

--- classes/nprml.php
+++ classes/nprml.php
@@ -35,27 +35,33 @@ function nprstory_post_to_nprml_story( $post ) {
     
     $teaser_text = '';
     if ( ! empty( $post->post_excerpt ) ){
-    	$teaser_text = $post->post_excerpt;
+        $teaser_text = $post->post_excerpt;
     }
     
+/*
     $custom_content_meta = get_option( 'ds_npr_api_mapping_body' );
     if ( $use_custom && ! empty( $custom_content_meta ) && $custom_content_meta != '#NONE#' && in_array( $custom_content_meta,$post_metas ) ){
         $content = get_post_meta( $post->ID, $custom_content_meta, true);
-    	$post_for_teaser = $post;
-    	$post_for_teaser->post_content = $content;
+        $post_for_teaser = $post;
+        $post_for_teaser->post_content = $content;
         if ( empty( $teaser_text ) ){
-	    	$teaser_text = nprstory_nai_get_excerpt( $post_for_teaser );
-    	}
+            $teaser_text = nprstory_nai_get_excerpt( $post_for_teaser );
+        }
     } else {
-	    $content = $post->post_content;
+*/
+        $content = $post->post_content;
 
         if( function_exists('wamu_markdown_transform')) {
             $content = wamu_markdown_transform($content);
         }
 
-        if ( function_exists('wamu_get_guests') ) {
-            $guests = wamu_get_guests($post->ID);
 
+        //error_log(wamu_get_guests($post->ID));
+
+        if ( function_exists('wamu_get_guests') ) {
+            
+            $guests = wamu_get_guests($post->ID);
+            
             if ( count($guests) > 0 ) {
                 $content .= '<p><h4>Guests</h4></p>';
             }
@@ -74,15 +80,15 @@ function nprstory_post_to_nprml_story( $post ) {
         $content .= '</p>';
 
 
-	    if ( empty( $teaser_text ) ) {
-		    $teaser_text = nprstory_nai_get_excerpt( $post );
-	    }
-    }
+        if ( empty( $teaser_text ) ) {
+            $teaser_text = nprstory_nai_get_excerpt( $post );
+        }
+    //}
     //lets see if there are any plugins that need to fix their shortcodes before we run do_shortcode
     if ( has_filter( 'npr_ds_shortcode_filter' ) ) {
-	    $content = apply_filters( 'npr_ds_shortcode_filter', $content );
+        $content = apply_filters( 'npr_ds_shortcode_filter', $content );
     }
-	  //let any plugin that has short codes try and replace those with HTML
+      //let any plugin that has short codes try and replace those with HTML
     $content = do_shortcode( $content );
     //for any remaining short codes, nuke 'em
     $content = strip_shortcodes( $content );
@@ -95,15 +101,15 @@ function nprstory_post_to_nprml_story( $post ) {
     $custom_title_meta = get_option( 'ds_npr_api_mapping_title' );
     if ( $use_custom && !empty( $custom_title_meta ) && $custom_title_meta != '#NONE#' && in_array( $custom_content_meta,$post_metas ) ){
         $custom_title = get_post_meta( $post->ID, $custom_title_meta, true );
-    	$story[] = array(
-	        'tag' => 'title',
-	        'text' => $custom_title,
-	    );
+        $story[] = array(
+            'tag' => 'title',
+            'text' => $custom_title,
+        );
     } else {
-	    $story[] = array(
-	        'tag' => 'title',
-	        'text' => $post->post_title,
-	    );
+        $story[] = array(
+            'tag' => 'title',
+            'text' => $post->post_title,
+        );
     }
     
     /**
@@ -121,22 +127,22 @@ function nprstory_post_to_nprml_story( $post ) {
     // $custom_byline_meta = get_option( 'ds_npr_api_mapping_byline' );
     // if ($use_custom && ! empty( $custom_byline_meta ) && $custom_byline_meta != '#NONE#' && in_array( $custom_content_meta,$post_metas ) ) {
     //     $byline = TRUE;
-    // 	$story[] = array(
-	   //      'tag' => 'byline',
+    //  $story[] = array(
+       //      'tag' => 'byline',
     //         'children' => array(
     //             array(
     //                 'tag' => 'name',
     //                 'text' => get_post_meta( $post->ID, $custom_byline_meta, true ),
     //             )
     //         ),
-	   // );
+       // );
     // }
 
 
     // if ( function_exists( 'get_coauthors' ) ) {
     //     $coauthors = get_coauthors( $post->ID );
     //     if ( ! empty( $coauthors ) ) {
-    //         $byline = TRUE;		
+    //         $byline = TRUE;      
     //         foreach( $coauthors as $i=>$co ) {
     //             $story[] = array(
     //                 'tag' => 'byline',
@@ -147,13 +153,13 @@ function nprstory_post_to_nprml_story( $post ) {
     //                     )
     //                 )
     //             );
-    //         }		
+    //         }        
     //     } else {
     //         nprstory_error_log( 'we do not have co authors' );
-    // 	}
+    //  }
     // } else {
-   	// 	nprstory_error_log('can not find get_coauthors');
-   	// }    
+    //  nprstory_error_log('can not find get_coauthors');
+    // }    
     // if ( ! $byline ) {
     //     $story[] = array(
     //         'tag' => 'byline',
@@ -161,13 +167,13 @@ function nprstory_post_to_nprml_story( $post ) {
     //             array(
     //                 'tag' => 'name',
     //                 'text' => get_the_author_meta( 'display_name', $post->post_author ),
-				// )
-    //         ),			
+                // )
+    //         ),           
     //     );
     // }
 
     // NPR One
-	// If the box is checked, the value here is '1'
+    // If the box is checked, the value here is '1'
     if ( ! empty( $_POST['send_to_nprone'] ) ) {
         $story[] = array(
             'tag' => 'parent',
@@ -209,15 +215,15 @@ function nprstory_post_to_nprml_story( $post ) {
 
     $perms_group = get_option( 'ds_npr_story_default_permission' );
     if (!empty( $perms_group ) ) {
-     	$story[] = array(
+        $story[] = array(
             'tag' => 'permissions',
             'children' => array (
                 array( 
-    				'tag' => 'permGroup',
-			     	'attr' => array( 'id' => $perms_group ),
-    			)
+                    'tag' => 'permGroup',
+                    'attr' => array( 'id' => $perms_group ),
+                )
             ),
-    	);
+        );
     }
     
     $custom_media_credit = get_option( 'ds_npr_api_mapping_media_credit' );
@@ -237,21 +243,21 @@ function nprstory_post_to_nprml_story( $post ) {
 
     $images = get_children( $args );
     $primary_image = get_post_thumbnail_id( $post->ID, 'full' );
-			
+            
     foreach ( $images as $image ) {
         $custom_credit = '';
         $custom_agency = '';
         $image_metas = get_post_custom_keys( $image->ID );
-	    if ( $use_custom && !empty( $custom_media_credit ) && $custom_media_credit != '#NONE#' && in_array( $custom_media_credit,$image_metas ) ) {
-	    	$custom_credit = get_post_meta( $image->ID, $custom_media_credit, true );
-	    }
+        if ( $use_custom && !empty( $custom_media_credit ) && $custom_media_credit != '#NONE#' && in_array( $custom_media_credit,$image_metas ) ) {
+            $custom_credit = get_post_meta( $image->ID, $custom_media_credit, true );
+        }
         if ( $use_custom && ! empty( $custom_media_agency ) && $custom_media_agency != '#NONE#' && in_array( $custom_media_agency,$image_metas ) ) {
-	    	$custom_agency = get_post_meta( $image->ID, $custom_media_agency, true);
-	    }
-	    
+            $custom_agency = get_post_meta( $image->ID, $custom_media_agency, true);
+        }
+        
         if ( $use_custom && !empty( $dist_media_option ) && $dist_media_option != '#NONE#' && in_array( $dist_media_option,$image_metas ) ) {
-	    	$dist_media = get_post_meta( $image->ID, $dist_media_option, true );
-	    }
+            $dist_media = get_post_meta( $image->ID, $dist_media_option, true );
+        }
 
         //if the image field for distribute is set and polarity then send it.
         //all kinds of other math when polarity is negative or the field isn't set.
@@ -259,43 +265,43 @@ function nprstory_post_to_nprml_story( $post ) {
         if ( $image->ID == $primary_image ) {
             $image_type = 'primary';
         }
-			
+            
         //is the image in the content?  If so, tell the APi with a flag that CorePublisher knows
         //WP may add something like "-150X150" to the end of the filename, before the extension.  Isn't that nice?
     //     $image_name_parts = split( ".", $image_guid );
     //     $image_regex = "/" . $image_name_parts[0] . "\-[a-zA-Z0-9]*" . $image_name_parts[1] . "/"; 
     //     $in_body = "";
     //     if ( preg_match( $image_regex, $content ) ) {
-				// if ( strstr( $image->guid, '?') ) {
-				// 	$in_body = "&origin=body";
-				// } else {
-				// 	$in_body = "?origin=body";
-				// }
+                // if ( strstr( $image->guid, '?') ) {
+                //  $in_body = "&origin=body";
+                // } else {
+                //  $in_body = "?origin=body";
+                // }
     //     }
         $story[] = array( 
             'tag' => 'image',
-            'attr' => array( 'src' => $image->guid . $in_body, 'type' => $image_type ), 
+            'attr' => array( 'src' => wamu_cdn_dirty_rewrite($image->guid) . $in_body, 'type' => $image_type ), 
             'children' => array(
                 array(
-				    'tag' => 'title',
-				    'text' => $image->post_title,
-				),
-				array(
-				    'tag' => 'caption',
-				    'text' => $image->post_excerpt,
- 			    ),
- 			    array(
- 			        'tag' => 'producer',  
- 			        'text' => $custom_credit
- 			    ),
- 			    array(
- 			        'tag' => 'provider',  
- 			        'text' => $custom_agency
- 			    )
+                    'tag' => 'title',
+                    'text' => $image->post_title,
+                ),
+                array(
+                    'tag' => 'caption',
+                    'text' => $image->post_excerpt,
+                ),
+                array(
+                    'tag' => 'producer',  
+                    'text' => $custom_credit
+                ),
+                array(
+                    'tag' => 'provider',  
+                    'text' => $custom_agency
+                )
             ),
         );
     }
-			
+            
     // //should be able to do the same as image for audio, with post_mime_type = 'audio' or something.
     // $args = array(
     //     'order'=> 'DESC',
@@ -303,9 +309,9 @@ function nprstory_post_to_nprml_story( $post ) {
     //     'post_parent' => $post->ID,
     //     'post_status' => null,
     //     'post_type' => 'attachment'
-    // );		
+    // );       
     // $audios = get_children( $args );
-			
+            
     // foreach ( $audios as $audio ) {
     //     $audio_meta = wp_get_attachment_metadata( $audio->ID );
     //     $caption = $audio->post_excerpt;
@@ -313,7 +319,7 @@ function nprstory_post_to_nprml_story( $post ) {
     //     if ( empty( $caption ) ) {
     //         $caption = $audio->post_content;
     //     }
-			
+            
     //     $story[] = array(
     //         'tag' => 'audio',
     //         'children' => array(
@@ -321,7 +327,7 @@ function nprstory_post_to_nprml_story( $post ) {
     //                 'tag' => 'format',
     //                 'children' => array (
     //                     array(
-    //         	           'tag' => 'mp3',
+    //                     'tag' => 'mp3',
     //                         'text' => $audio->guid,
     //                     )
     //                 ),
@@ -335,7 +341,7 @@ function nprstory_post_to_nprml_story( $post ) {
     //                 'text' => $audio_meta['length'],
     //             ),
     //         ),
-    //     );				
+    //     );               
     // }
 
     $audio_file = get_post_meta( $post->ID, 'audio_file', true );
@@ -373,11 +379,11 @@ function nprstory_post_to_nprml_story( $post ) {
         // re-worked if we need to accomodate other plug-ins that use enclosures.
         foreach( $enclosures as $enclosure ) {
             $pieces = explode( "\n", $enclosure );
-		    if ( !empty( $pieces[3] ) ) {
+            if ( !empty( $pieces[3] ) ) {
                 $metadata = unserialize( $pieces[3] );
                 $duration = ( ! empty($metadata['duration'] ) ) ? nprstory_convert_duration_to_seconds( $metadata['duration'] ) : NULL;
-		    }
-		    $story[] = array(
+            }
+            $story[] = array(
                 'tag' => 'audio',
                 'children' => array(
                     array(
@@ -454,7 +460,7 @@ function nprstory_nprml_array_to_xml( $tag, $attrs, $data ) {
 function nprstory_nprml_item_to_xml( $item, $xml ) {
     if ( ! array_key_exists( 'tag', $item ) ) {
         error_log( "Unable to convert NPRML item to XML: no tag for: " . print_r( $item, true ) ); // debug use
-		// this should actually be a serious error
+        // this should actually be a serious error
     }
     $elem = $xml->createElement( $item[ 'tag' ] );
     if ( array_key_exists( 'children', $item ) ) {
